c1857acf7d2613b03fc2102ae374d3d909fe231c
Yellofishy <32941977+Yellofishy@users.noreply.github.com>
2020-03-13 09:08:48 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,101 @@
+import time
+import random
+
+# 28 characters on screen at once, we need to add a refresh system where everything on screen is recorded
+
+
+points = 20
+tiles = []
+for i in range(28):
+    print(i)
+
+    tiles.append("▉              ▉               ▉               ▉               ▉")
+print("I will eat you.")
+
+##time.sleep (speed)
+speed = 10 / points
+run = True
+skip = 0
+'''for i in range(28):
+  if skip != 0: 
+    skip = skip - 1
+  x = random.randrange (1,10)
+  if x == 1:
+    for d in range(6):
+      try:  
+        tiles[i+d] = ("▉**************▉               ▉               ▉               ▉")
+      except:
+        pass
+      skip = 5
+  elif x == 2:
+    for d in range(6):
+      try:  
+        tiles[i+d] = ("▉              ▉***************▉               ▉               ▉")
+      except:
+        pass
+    skip = 5
+  elif x == 3:
+    for d in range(6):
+      try:  
+        tiles[i+d] = ("▉              ▉               ▉***************▉               ▉")
+      except:
+        pass
+    skip = 5
+  elif x == 4:
+    for d in range(6):
+      try:  
+        tiles[i+d] = ("▉              ▉               ▉               ▉***************▉")
+      except:
+        pass
+    skip = 5
+
+  else: 
+    tiles[i] = ("▉              ▉               ▉               ▉               ▉")'''
+
+for i in range(len(tiles)):
+    tiles[i] = "▉              ▉               ▉               ▉               ▉"
+
+skip = -1
+while run == True:
+
+    for i in range(len(tiles)):
+        try:
+            tiles[i] = tiles[i + 1]
+        except:
+            tiles[i] = ""
+    if skip > 0:
+        skip = skip - 1
+        print(str(skip) + "Are you hallucinating")
+    else:
+        skip = -1
+        print("hallucinations")
+        x = random.randrange(1, 10)
+    if x == 1:
+        tiles[27] = ("▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉               ▉               ▉               ▉")
+        if skip < 0:
+            skip = 5
+
+    elif x == 2:
+        tiles[27] = ("▉              ▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉               ▉               ▉")
+        if skip < 0:
+            skip = 5
+
+    elif x == 3:
+        tiles[27] = ("▉              ▉               ▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉               ▉")
+        if skip < 0:
+            skip = 5
+
+    elif x == 4:
+        tiles[27] = ("▉              ▉               ▉               ▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉")
+        if skip < 0:
+            skip = 5
+
+    else:
+        tiles[27] = ("▉              ▉               ▉               ▉               ▉")
+
+
+    for i in range(len(tiles)):
+        print(tiles[i])
+    time.sleep(speed)
+
+
